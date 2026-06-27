b3fc2fc0b19517a50aa076a36195d6c7c157c346
M1wkv <alexandermuwka@gmail.com>
2026-06-27 17:19:20 +0500

    Refresh portfolio runtime cache versions

--- supabase-loader.js
+++ supabase-loader.js
@@ -1,3 +1,4 @@
+
 (async () => {
   const STORAGE_ASSETS = "portfolioSphere.assets";
   const STORAGE_CV = "portfolioSphere.cvNodes";
@@ -146,7 +147,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260627-project-ui-4";
+    script.src = "sphere.js?v=20260627-project-ui-5";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
