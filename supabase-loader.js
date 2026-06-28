68485991aafb147d2c77c173b3c276267728c0b2
M1wkv <alexandermuwka@gmail.com>
2026-06-28 17:43:15 +0500

    Load the CV cylinder runtime

--- supabase-loader.js
+++ supabase-loader.js
@@ -157,7 +157,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260628-sphere-count-1";
+    script.src = "sphere.js?v=20260628-cv-cylinder-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
