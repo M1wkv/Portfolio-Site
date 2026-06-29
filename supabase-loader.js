8dc053eefd411bff6149a92a1c7d1c8c2cbcad51
M1wkv <alexandermuwka@gmail.com>
2026-06-30 01:28:29 +0500

    Repair project ribbon cache in supabase-loader.js

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260630-project-ribbon-2";
+    script.src = "sphere.js?v=20260630-project-ribbon-3";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
