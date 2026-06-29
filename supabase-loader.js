61ceeee6c1ccf35b326f5087079ca1e85823a97e
M1wkv <alexandermuwka@gmail.com>
2026-06-29 20:43:01 +0500

    Restore supabase-loader.js from last working site snapshot

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere-runtime.js?v=20260629-repair-1";
+    script.src = "sphere.js?v=20260628-optimize-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
@@ -220,3 +220,4 @@
   });
   window.setInterval(refreshIfContentChanged, 15000);
 })();
+
