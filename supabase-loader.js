d0e4a10cc5c26cf6252561079454dc9d7b82d788
M1wkv <alexandermuwka@gmail.com>
2026-07-04 01:03:40 +0500

    Force reference glass plate deployment

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260704-reference-glass-1";
+    script.src = "sphere.js?v=20260704-reference-glass-2";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
