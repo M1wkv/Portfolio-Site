e689e05d2bf91a17cd10fdd023b9f6a2e432bcec
M1wkv <alexandermuwka@gmail.com>
2026-07-03 21:55:09 +0500

    Force final glass frost deployment

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260703-glass-frost-1";
+    script.src = "sphere.js?v=20260703-glass-frost-2";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
