06eaf13e0e35f462572ce585d7853284ed1c5d83
M1wkv <alexandermuwka@gmail.com>
2026-07-03 21:52:44 +0500

    Apply frost to refracted glass imagery

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260703-glass-dispersion-1";
+    script.src = "sphere.js?v=20260703-glass-frost-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
