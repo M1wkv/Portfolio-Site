4ffe0d4a0f928590abace4e5362fe8c10be19651
M1wkv <alexandermuwka@gmail.com>
2026-07-03 21:22:19 +0500

    Add center-to-edge glass refraction

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260703-glass-controls-2";
+    script.src = "sphere.js?v=20260703-glass-refraction-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
