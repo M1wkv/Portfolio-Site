728ef379762b8cce50a59d499fd1c81fe531a834
M1wkv <alexandermuwka@gmail.com>
2026-07-05 01:12:22 +0500

    Refresh water effect assets in supabase-loader.js

--- supabase-loader.js
+++ supabase-loader.js
@@ -1,3 +1,4 @@
+
 (async () => {
   const STORAGE_ASSETS = "portfolioSphere.assets";
   const STORAGE_CV = "portfolioSphere.cvNodes";
@@ -162,7 +163,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260705-water-effects-1";
+    script.src = "sphere.js?v=20260705-water-effects-2";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
