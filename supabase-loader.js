987fa2b0efcb49bf2f63b47a3f9ae4690aabe5b2
M1wkv <alexandermuwka@gmail.com>
2026-07-05 00:43:35 +0500

    Force smooth full-frost glass deployment

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260705-glass-smooth-1";
+    script.src = "sphere.js?v=20260705-glass-smooth-2";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
