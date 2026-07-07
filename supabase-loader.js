0f0d4499d0447bbe99f75ca1a1afb4917602b7ef
M1wkv <alexandermuwka@gmail.com>
2026-07-08 02:11:40 +0500

    Revert latest project spacing change in supabase-loader.js

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260708-center-gap-3";
+    script.src = "sphere.js?v=20260708-center-gap-4-revert";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
