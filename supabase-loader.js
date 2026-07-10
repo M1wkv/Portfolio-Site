c8774666d75c32b1024d16ba4007554f52ffbde9
M1wkv <alexandermuwka@gmail.com>
2026-07-10 21:43:51 +0500

    Add controlled type warp to CV cube: supabase-loader.js

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260710-cv-cube-readable-1";
+    script.src = "sphere.js?v=20260710-cv-cube-type-warp-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
