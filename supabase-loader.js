6cbafba583151773a6536739b047c17ea15034a3
M1wkv <alexandermuwka@gmail.com>
2026-07-12 02:42:23 +0500

    Add idle panel effects controls

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260712-project-strip-restore-1";
+    script.src = "sphere.js?v=20260712-idle-panels-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
