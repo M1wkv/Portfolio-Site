f81d7e13853b29dfdc864bbcafd663c0dc2402d4
M1wkv <alexandermuwka@gmail.com>
2026-07-12 01:40:15 +0500

    Add panel frost control: supabase-loader.js

--- supabase-loader.js
+++ supabase-loader.js
@@ -162,7 +162,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260712-panel-controls-1";
+    script.src = "sphere.js?v=20260712-frost-control-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
