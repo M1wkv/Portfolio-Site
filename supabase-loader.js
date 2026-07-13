499491bb52bd09df1d6954ecd07d3973c1ce346a
M1wkv <alexandermuwka@gmail.com>
2026-07-14 02:04:51 +0500

    Add responsive sphere and cylinder presets

--- supabase-loader.js
+++ supabase-loader.js
@@ -182,7 +182,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260714-project-scale-centered-range-1";
+    script.src = "sphere.js?v=20260714-responsive-sphere-cylinder-presets-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
@@ -243,6 +243,7 @@
   });
   window.setInterval(refreshIfContentChanged, 15000);
 })();
+
 
 
 
