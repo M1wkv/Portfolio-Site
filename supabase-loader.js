867f47e3fb7bfdd4c71e654273da9319ba911b43
M1wkv <alexandermuwka@gmail.com>
2026-07-14 01:15:58 +0500

    Remove CV background sphere blur

--- supabase-loader.js
+++ supabase-loader.js
@@ -182,7 +182,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260713-project-gap-name-1";
+    script.src = "sphere.js?v=20260714-cv-sphere-no-blur-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
@@ -243,3 +243,4 @@
   });
   window.setInterval(refreshIfContentChanged, 15000);
 })();
+
