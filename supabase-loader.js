74718c546198f6d7db720d67994d86ce2d258ba5
M1wkv <alexandermuwka@gmail.com>
2026-08-12 21:17:53 +0500

    Keep opened project media below navigation

--- supabase-loader.js
+++ supabase-loader.js
@@ -188,7 +188,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260812-project-chip-hover-3";
+    script.src = "sphere.js?v=20260812-project-media-safe-area-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
