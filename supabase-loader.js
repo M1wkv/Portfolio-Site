5ca2aceae1edd33c1415218d513c960d49eaf27b
M1wkv <alexandermuwka@gmail.com>
2026-08-13 12:40:41 +0500

    Add portfolio visitor analytics

--- supabase-loader.js
+++ supabase-loader.js
@@ -188,7 +188,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260812-stable-topbar-1";
+    script.src = "sphere.js?v=20260813-analytics-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
