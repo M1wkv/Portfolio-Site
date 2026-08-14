9734b2150ed19cfd544464dff8e2824b7365138b
M1wkv <alexandermuwka@gmail.com>
2026-08-14 17:50:38 +0500

    Restore previous sphere cache version

--- supabase-loader.js
+++ supabase-loader.js
@@ -188,7 +188,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260814-project-back-1";
+    script.src = "sphere.js?v=20260813-progressive-images-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
