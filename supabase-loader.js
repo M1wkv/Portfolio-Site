7cab50a2b26f8b490d252f51a18f83c59eb400db
M1wkv <alexandermuwka@gmail.com>
2026-08-29 21:30:25 +0500

    Bump sphere video runtime cache

--- supabase-loader.js
+++ supabase-loader.js
@@ -188,7 +188,7 @@
   function loadSphereScript() {
     const script = document.createElement("script");
     script.async = false;
-    script.src = "sphere.js?v=20260827-click-center-1";
+    script.src = "sphere.js?v=20260829-video-media-1";
     script.onload = () => {
       document.documentElement.dataset.sphereScriptLoaded = "true";
     };
